aa2a431218c2945eba89e91bd44e9a8ccfc63015
Bailey Frank <bfrank@unomaha.edu>
2022-04-24 20:06:17 -0500

    Changed string values of self.__status to boolean values instead of string values

--- classes.py
+++ classes.py
@@ -9,21 +9,20 @@ class Television:
     MIN_VOLUME = 0      # Minimum TV volume
     MAX_VOLUME = 2      # Maximum TV volume
 
-    def __init__(self, __channel: int, __volume: int, __status: str) -> None:
+    def __init__(self, __channel: int, __volume: int, __status: bool) -> None:
 
         """
         Constructor to create initial state of the television object, and to store private variables and their
         parameters throughout file
 
-        :param status:
         :param self.__channel: the number associated with a particular television station
         :param self.__volume: the number associated with the level of noise coming from the television
-        :param self.__status: the boolean value of 'on' or 'off', which describes if the television is powered
+        :param self.__status: the boolean value of 'True' or 'False', which describes if the television is powered
         """
 
         self.__channel: int = 0
         self.__volume: int = 0
-        self.__status: str = 'Off'
+        self.__status: bool = False
 
     def power(self) -> None:
 
@@ -31,10 +30,10 @@ class Television:
         Method to activate/deactivate the television's power when called
         """
 
-        if self.__status == 'On':
-            self.__status = 'Off'
+        if self.__status is True:
+            self.__status = False
         else:
-            self.__status = "On"
+            self.__status = True
 
     def channel_up(self) -> None:
 
@@ -43,7 +42,7 @@ class Television:
         maximum value when called
         """
 
-        if self.__status == "On":
+        if self.__status is True:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
             else:
@@ -56,7 +55,7 @@ class Television:
         minimum value when called
         """
 
-        if self.__status == "On":
+        if self.__status is True:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
             else:
@@ -69,7 +68,7 @@ class Television:
         already at maximum value when called
         """
 
-        if self.__status == "On":
+        if self.__status is True:
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
@@ -80,7 +79,7 @@ class Television:
         already at minimum value when called
         """
 
-        if self.__status == "On":
+        if self.__status is True:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
@@ -96,7 +95,7 @@ class Television:
         """
 
         stat = False
-        if self.__status == "On":
+        if self.__status is True:
             stat = True
 
         return f'TV Status: Is on = {stat}, Channel = {self.__channel}, Volume = {self.__volume}'
--- test_classes.py
+++ test_classes.py
@@ -1,36 +1,42 @@
-import unittest
+import pytest
 from classes import *
 
+def setup_method(self):
+    self.tv1 = Television()
 
-class MyTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.pow = Television(self.__init__)
-        self.ch = Television(self.__init__)
-        self.vol = Television(self.__init__)
-
-    def tearDown(self):
-        del self.pow
-        del self.ch
-        del self.vol
-
-    def test_init(self):
-        self.assertEqual(self.__str__(), 'TV Status: Is on = False, Channel = 0, Volume = 0')
-
-     def test_power(self):
-         self.assertEqual(self.pow.power(), 'On', msg='Not True')
-
-     def test_channel_up(self):
-         self.assertEqual(self.ch.channel_up(), 1, msg='Did not increase channel correctly')
-
-    def test_channel_down(self):
-         self.assertEqual(self.vol.channel_down(), 2, msg='Did not decrease channel correctly')
-
-     def test_volume_up(self):
-         self.assertEqual(self.vol.volume_up(), 1, msg='Did not increase volume correctly')
-
-     def test_volume_down(self):
-         self.assertEqual(self.vol.volume_down(), 0, msg='Did not decrease volume correctly')
-
-if __name__ == '__main__':
-     unittest.main()
+# import unittest
+# from classes import *
+#
+#
+# class MyTestCase(unittest.TestCase):
+#
+#     def setUp(self):
+#         self.pow = Television(self.__init__)
+#         self.ch = Television(self.__init__)
+#         self.vol = Television(self.__init__)
+#
+#     def tearDown(self):
+#         del self.pow
+#         del self.ch
+#         del self.vol
+#
+#     def test_init(self):
+#         self.assertEqual(self.__str__(), 'TV Status: Is on = False, Channel = 0, Volume = 0')
+#
+#      def test_power(self):
+#          self.assertEqual(self.pow.power(), 'On', msg='Not True')
+#
+#      def test_channel_up(self):
+#          self.assertEqual(self.ch.channel_up(), 1, msg='Did not increase channel correctly')
+#
+#     def test_channel_down(self):
+#          self.assertEqual(self.vol.channel_down(), 2, msg='Did not decrease channel correctly')
+#
+#      def test_volume_up(self):
+#          self.assertEqual(self.vol.volume_up(), 1, msg='Did not increase volume correctly')
+#
+#      def test_volume_down(self):
+#          self.assertEqual(self.vol.volume_down(), 0, msg='Did not decrease volume correctly')
+#
+# if __name__ == '__main__':
+#      unittest.main()

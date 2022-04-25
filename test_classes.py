386f375768d86470d18a11d8c47affbe05771839
Bailey Frank <bfrank@unomaha.edu>
2022-04-24 21:29:19 -0500

    Added return statements to functions & edited docstrings for volume & channel functions

--- test_classes.py
+++ test_classes.py
@@ -1,42 +1,60 @@
-import pytest
 from classes import *
 
-def setup_method(self):
-    self.tv1 = Television()
 
-# import unittest
-# from classes import *
-#
-#
-# class MyTestCase(unittest.TestCase):
-#
-#     def setUp(self):
-#         self.pow = Television(self.__init__)
-#         self.ch = Television(self.__init__)
-#         self.vol = Television(self.__init__)
-#
-#     def tearDown(self):
-#         del self.pow
-#         del self.ch
-#         del self.vol
-#
-#     def test_init(self):
-#         self.assertEqual(self.__str__(), 'TV Status: Is on = False, Channel = 0, Volume = 0')
-#
-#      def test_power(self):
-#          self.assertEqual(self.pow.power(), 'On', msg='Not True')
-#
-#      def test_channel_up(self):
-#          self.assertEqual(self.ch.channel_up(), 1, msg='Did not increase channel correctly')
-#
-#     def test_channel_down(self):
-#          self.assertEqual(self.vol.channel_down(), 2, msg='Did not decrease channel correctly')
-#
-#      def test_volume_up(self):
-#          self.assertEqual(self.vol.volume_up(), 1, msg='Did not increase volume correctly')
-#
-#      def test_volume_down(self):
-#          self.assertEqual(self.vol.volume_down(), 0, msg='Did not decrease volume correctly')
-#
-# if __name__ == '__main__':
-#      unittest.main()
+class Test:
+    def setup_method(self):
+        self.tvOffMin = Television(0, 0, False)
+        self.tvOnMid = Television(1, 1, True)
+        self.tvOnMax = Television(3, 2, True)
+        self.tvOnMin = Television(0, 0, True)
+        self.tvOffMid = Television(1, 1, False)
+
+    def teardown_method(self):
+        del self.tvOffMin
+        del self.tvOnMid
+        del self.tvOnMax
+        del self.tvOnMin
+        del self.tvOffMid
+
+    def test_power(self):
+        assert self.tvOffMin.power() is True
+        assert self.tvOnMid.power() is False
+        assert self.tvOnMax.power() is False
+        assert self.tvOnMin.power() is False
+        assert self.tvOffMid.power() is True
+
+    def test_channel_up(self):
+        assert self.tvOffMin.channel_up() == 0
+        assert self.tvOnMid.channel_up() == 2
+        assert self.tvOnMax.channel_up() == 0
+        assert self.tvOnMin.channel_up() == 1
+        assert self.tvOffMid.channel_up() == 1
+
+    def test_channel_down(self):
+        assert self.tvOffMin.channel_down() == 0
+        assert self.tvOnMid.channel_down() == 0
+        assert self.tvOnMax.channel_down() == 2
+        assert self.tvOnMin.channel_down() == 3
+        assert self.tvOffMid.channel_down() == 1
+
+    def test_volume_up(self):
+        assert self.tvOffMin.volume_up() == 0
+        assert self.tvOnMid.volume_up() == 2
+        assert self.tvOnMax.volume_up() == 2
+        assert self.tvOnMin.volume_up() == 1
+        assert self.tvOffMid.volume_up() == 1
+
+    def test_volume_down(self):
+        assert self.tvOffMin.volume_down() == 0
+        assert self.tvOnMid.volume_down() == 0
+        assert self.tvOnMax.volume_down() == 1
+        assert self.tvOnMin.volume_down() == 0
+        assert self.tvOffMid.volume_down() == 1
+
+    def test_str(self):
+        assert self.tvOffMin.__str__() == "TV Status: Is on = False, Channel = 0, Volume = 0"
+        assert self.tvOnMid.__str__() == "TV Status: Is on = True, Channel = 1, Volume = 1"
+        assert self.tvOnMax.__str__() == "TV Status: Is on = True, Channel = 3, Volume = 2"
+        assert self.tvOnMin.__str__() == "TV Status: Is on = True, Channel = 0, Volume = 0"
+        assert self.tvOffMid.__str__() == "TV Status: Is on = False, Channel = 1, Volume = 1"
+
